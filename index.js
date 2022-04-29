1a142b14d9979da77ded5158198178bda701c849
Filipe Mazzon <filipe.mazzon.ribeiro@gmail.com>
2022-04-29 17:46:43 -0300

    :fire: add delete others domain for now

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const {buildSchema} = require('graphql');
-const {DbCollectionEnum} = require("./src/infraestructure/dbCollection.enum");
-
-
 
 const db = {
 	products: [
@@ -73,76 +70,8 @@ const schema = buildSchema(`
 	}
 `);
 
-const calculateIndex = (index) => {
-	return (parseInt(index, 10) + 1).toString();
-}
-
-
-
-const aggregateProduct = (product) => {
-	return {
-		...product,
-		categories: product.categories.map((category) => {
-			return db.categories.find(categoryInList => categoryInList.id === category);
-		})
-	}
-}
-const handleProduct = (productSearch) => {
-	const productFound = db.products.find(product => product.id === productSearch);
-	if (productFound.categories?.length) {
-		return aggregateProduct(productFound);
-	}
-	return productFound;
-}
-
-const calculateTotalPrice = (products) => {
-	return products.reduce((totalPrice, product) => totalPrice + product.price, 0);
-}
-
-const aggregateOrder = (order) => {
-	const products = order.products.map(handleProduct);
-	return {
-		...order,
-		products,
-		totalPrice: calculateTotalPrice(products)
-	}
-}
-
-const createProduct = ({name, categories, price}) => {
-	const id = getId(DbCollectionEnum.products);
-	validateIfExists(DbCollectionEnum.categories, categories);
-	const product = {
-		id,
-		name: name,
-		categories: categories,
-		price: price
-	}
-	db.products.push(product);
-	return aggregateProduct(product);
-}
-
-const createOrder = (order) => {
-	const id = getId(DbCollectionEnum.orders);
-	validateIfExists(DbCollectionEnum.products, order.products);
-	const newOrder = {
-		id,
-		products: order.products
-	}
-	db.orders.push(newOrder)
-	return aggregateOrder(newOrder);
-}
 const root = {
-	hello: () => {
-		return 'Hello World!';
-	},
-	products: () => {
-		return db.products.map(aggregateProduct);
-	},
-	orders: () => {
-		return db.orders.map(aggregateOrder);
-	},
-	createProduct,
-	createOrder
+
 };
 
 const app = express();
